c73d63a9cd8af6260af970e3f469c12da60c631a
Skyne <skyne9@gmail.com>
2025-03-27 21:09:16 +0100

    added support for fan_generic type fans

--- calibrate_fan.py
+++ calibrate_fan.py
@@ -1,5 +1,7 @@
 import os, time
 
+import configfile
+
 class CalibrateFan:
     def __init__(self, config):
         self.printer = config.get_printer()
@@ -11,6 +13,7 @@ class CalibrateFan:
         self.current_gcmd = None
         self.calibration_active = False
         self.sample_timer = None 
+        self.fan = None
         self.fan_name = None
 
         self.measure_per_step = 3
@@ -28,44 +31,49 @@ class CalibrateFan:
 
     def cmd_CALIBRATE_FAN(self, gcmd):
         if self.calibration_active:
-            gcmd.respond_error("Calibration already in progress")
+            gcmd.respond_info("Calibration already in progress")
             return
+        
+        self._reset_state()
+
         self.current_gcmd = gcmd
         self.calibration_active = True
         
         steps = int(gcmd.get('STEPS', 10))
         self.measure_per_step = int(gcmd.get('MEASURE_PER_STEP', 3))
         self.fan_name = gcmd.get('FAN', 'fan')
-        fan = self.printer.lookup_object(self.fan_name)
-        if fan is None:
-            gcmd.respond_error("Fan not found")
+        self.fan = self._try_find_fan(self.fan_name)
+        if self.fan is None:
+            self.fan = self._try_find_fan('fan_generic %s' % self.fan_name)
+        if self.fan is None:
+            self.fan = self._try_find_fan('heater_fan %s' % self.fan_name)
+        if self.fan is None:
+            gcmd.respond_error("Fan %s not found" % self.fan_name)
+            self.calibration_active = False
             return
-
+        
         # Start calibration sequence
         gcmd.respond_info("Calibrating fan %s" % self.fan_name)
         gcmd.respond_info("Running fan from 0 to 100%% power in %d steps" % steps)
         
-        
-        # Initialize state
-        self.current_step = 0
-        self.current_measurement = 0
-        self.data = []
-        self.step_time = 3
-        self.steps = steps
-        self.initial_fanstop_issued = False
-        
         # Start the calibration process
         self.reactor.update_timer(self.sample_timer, self.reactor.NOW)
 
-    def _next_calibration_step(self, eventtime):
+    def _try_find_fan(self, fan_name):
+        try:
+            return self.printer.lookup_object(fan_name)
+        except configfile.error:
+            return None
+        
 
+    def _next_calibration_step(self, eventtime):
         if self.current_step == 0:
             fan_rpm = self._measure_fan_speed(eventtime, drop_result=self.current_step == 0)
 
             if fan_rpm is not None and fan_rpm > 0:
                 self.current_gcmd.respond_info("Fan is already spinning at %s, waiting for it to stop" % fan_rpm)
                 if not self.initial_fanstop_issued:
-                    self.gcode.run_script("M106 S0")
+                    self._set_fan_power(0)
                     self.initial_fanstop_issued = True
                 return self.reactor.monotonic() + 1
         
@@ -86,14 +94,13 @@ class CalibrateFan:
             
         # Calculate current power level
         current_power = (100 / self.steps ) * self.current_step
-        power_scaled = int((current_power/100) * 255)
+        power_scaled = current_power/100
         
         # Set fan power using proper command syntax
-        cmd_str = f"M106 S{power_scaled}"
-        self.current_gcmd.respond_info("Setting fan power to %d%% with command: %s" % (current_power, cmd_str))
-        self.gcode.run_script(cmd_str)
+        self.current_gcmd.respond_info("Setting fan power to %d%%" % (current_power))
+        self._set_fan_power(power_scaled)
         
-        self.current_gcmd.respond_info("Waiting for %d seconds" % self.step_time)
+        #self.current_gcmd.respond_info("Waiting for %d seconds" % self.step_time)
 
         current_eventtime = self.reactor.monotonic()
         return current_eventtime + self.step_time
@@ -102,8 +109,7 @@ class CalibrateFan:
         if not self.calibration_active:
             return None
 
-        fan = self.printer.lookup_object('fan')
-        status = fan.get_status(eventtime)
+        status = self.fan.get_status(eventtime)
         
         # Store data
         if not drop_result:
@@ -120,9 +126,8 @@ class CalibrateFan:
 
     def _calibration_complete(self):            
         #shutdown fan
-        cmd_str = f"M106 S0"
         self.current_gcmd.respond_info("Setting fan power to 0%")
-        self.gcode.run_script(cmd_str)
+        self._set_fan_power(0)
     
         self.current_gcmd.respond_info("Saving calibration data...")
         
@@ -130,9 +135,7 @@ class CalibrateFan:
 
         # Cleanup
         self.calibration_active = False
-        self.current_gcmd = None
-        self.initial_fanstop_issued = False
-        self.fan_name = None
+        self._reset_state()
 
     def _save_calibration_data(self):
 
@@ -145,10 +148,34 @@ class CalibrateFan:
 
         self.current_gcmd.respond_info("Calibration data saved to %s" % filename)
     
-    def get_filename(self, base, name_suffix, fan=None):
+    def _set_fan_power(self, power):
+        #check fan type if fan is Fan then use M106 if fan is GenericFan then use SET_FAN_SPEED        
+        if self.fan.__class__.__name__ == 'Fan':
+            cmd_str = f"M106 S{power}"
+        elif self.fan.__class__.__name__ == 'PrinterFanGeneric':
+            power_scaled = int(power * 255)
+            cmd_str = f"SET_FAN_SPEED FAN={self.fan_name} SPEED={power_scaled}"
+        else:
+            self.current_gcmd.respond_info("Fan type not supported %s" % fan.__class__.__name__)
+            return
+
+        self.gcode.run_script(cmd_str)
+
+    def _reset_state(self):
+        self.current_step = 0
+        self.current_measurement = 0
+        self.data = []
+        self.step_time = 3
+        self.steps = 10
+        self.current_gcmd = None
+        self.initial_fanstop_issued = False
+        self.fan = None
+        self.fan_name = None
+
+    def get_filename(self, base, name_suffix, fan_name=None):
         name = base
-        if fan:
-            name += '_' + fan
+        if fan_name:
+            name += '_' + fan_name
         name += '_' + name_suffix
         return os.path.join("/tmp", name + ".csv")        
 
